f5598b6757dd5097184d03dfdfd14c924d10e947
Juan Pablo Lopez <jlopezes98@gmail.com>
2023-04-29 19:00:42 -0600

    Permutaciones estacionalidad

--- src/funciones/funciones_dlm_estacional_reordena.R
+++ src/funciones/funciones_dlm_estacional_reordena.R
@@ -2,7 +2,7 @@ library(LaplacesDemon)
 
 
 estimacion_MARSS <- function(y, F, inicio, periodos_extra, G, W, V, mt, Ct){
- 
+  
   U <- matrix(0, nrow = m, ncol = 1) ## 2x1; both elements = 0
   A <- matrix(0) ## 1x1; scalar = 0
   
@@ -16,16 +16,22 @@ estimacion_MARSS <- function(y, F, inicio, periodos_extra, G, W, V, mt, Ct){
   
   #G_salida <-  matrix(list(0), m, m)
   #diag(G_salida) <- dlm$par$B
-  G_salida <- matrix(c(dlm$par$B[1], 0, 0, 
-                       0, dlm$par$B[2], 0, 
-                       0, 0, 1), nrow=3)
+  G_salida <- matrix(c(dlm$par$B[1], 0, 0, 0,0,0,
+                       0, dlm$par$B[2], 0, 0,0,0,
+                       0, 0, 0,0,0,1,
+                       0,0,1,0,0,0,
+                       0,0,0,1,0,0,
+                       0,0,0,0,1,0), nrow=6)
   mode(G_salida) <- 'numeric'
   
   # W_salida <-  matrix(list(0), m, m)
   # diag(W_salida) <- dlm$par$Q
-  W_salida <- matrix(c(dlm$par$Q[1], 0, 0,
-                       0, dlm$par$Q[2], 0,
-                       0, 0, 0), nrow=3)
+  W_salida <- matrix(c(dlm$par$Q[1], 0, 0,0,0,0,
+                       0, dlm$par$Q[2], 0,0,0,0,
+                       0, 0, 0,0,0,0,
+                       0,0,0,0,0,0,
+                       0,0,0,0,0,0,
+                       0,0,0,0,0,0), nrow=6)
   mode(W_salida) <- 'numeric'
   
   V_salida <-  coef(dlm, type = "matrix")$R
@@ -43,9 +49,9 @@ actualizacion_dlm_estima_G_W <- function(y, variables_F, y_hist, variables_F_his
   error_varianza <- function(x) {
     if(any (diag(x)<0)) stop ('Elementos del parametro de escala o varianzas deben ser positivos')
   }
-
-  mt_hist <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/m1_m12.rds')
-  Ct_hist <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/C1_C12.rds')
+  
+  mt_hist <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/m1_m12.rds')
+  Ct_hist <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/C1_C12.rds')
   mt_menos_1 <- m0
   Ct_menos_1 <- C0
   lista_at <- list()
@@ -59,9 +65,9 @@ actualizacion_dlm_estima_G_W <- function(y, variables_F, y_hist, variables_F_his
   lista_CI_sup <- list()
   interv <- F
   for(t in 1:length(y)){
-  
-    mt_inicio <- matrix(unlist(mt_hist[length(mt_hist)-inicio+1]), nrow=3)
-    Ct_inicio <- matrix(unlist(Ct_hist[length(Ct_hist)-inicio+1]), nrow=3)
+    
+    mt_inicio <- matrix(unlist(mt_hist[length(mt_hist)-inicio+1]), nrow=6)
+    Ct_inicio <- matrix(unlist(Ct_hist[length(Ct_hist)-inicio+1]), nrow=6)
     
     estima_MARSS <- estimacion_MARSS(y_hist, variables_F_hist, inicio, t, G_forma,
                                      W_forma, V_forma, mt_inicio, Ct_inicio)
@@ -128,8 +134,8 @@ actualizacion_dlm_V_desc_estima_G_W <- function(y, variables_F, y_hist, variable
     if(any (diag(x)<0)) stop ('Elementos del parametro de escala o varianzas deben ser positivos')
   }
   
-  mt_hist <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/m1_m12.rds')
-  Ct_hist <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/C1_C12.rds')
+  mt_hist <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/m1_m12.rds')
+  Ct_hist <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/C1_C12.rds')
   mt_menos_1 <- m0
   Ct_menos_1 <- C0
   St_menos_1 <- S0
@@ -146,9 +152,9 @@ actualizacion_dlm_V_desc_estima_G_W <- function(y, variables_F, y_hist, variable
   lista_St <- list()
   interv <- F
   for(t in 1:length(y)){
-   
-    mt_inicio <- matrix(unlist(mt_hist[length(mt_hist)-inicio+1]), nrow=3)
-    Ct_inicio <- matrix(unlist(Ct_hist[length(Ct_hist)-inicio+1]), nrow=3)
+    
+    mt_inicio <- matrix(unlist(mt_hist[length(mt_hist)-inicio+1]), nrow=6)
+    Ct_inicio <- matrix(unlist(Ct_hist[length(Ct_hist)-inicio+1]), nrow=6)
     
     estima_MARSS <- estimacion_MARSS(y_hist, variables_F_hist, inicio, t, G_forma,
                                      W_forma, V = matrix(St_menos_1), mt_inicio, Ct_inicio)
@@ -203,145 +209,6 @@ actualizacion_dlm_V_desc_estima_G_W <- function(y, variables_F, y_hist, variable
     Ct_hist[[inicio + t]] <- Ct
     
     interv <- F
-  }
-  return(list("at" = lista_at,  "Rt" = lista_Rt,  "ft" = lista_ft, 
-              "Qt" = lista_Qt, "CI" = lista_CI, "CI_inf" = lista_CI_inf, 
-              "CI_sup" = lista_CI_sup, "mt" = lista_mt, "Ct" = lista_Ct,
-              "St" = lista_St))
-}
-
-
-
-################################################################################
-## Usando los valores iniciales
-##
-################################################################################
-
-
-
-estimacion_MARSS2 <- function(y, F, inicio, periodos_extra, G, W, V, mt, Ct){
-  
-  U <- matrix(0, nrow = m, ncol = 1) ## 2x1; both elements = 0
-  A <- matrix(0) ## 1x1; scalar = 0
-  
-  y_iter <- matrix(y[(periodos_extra+1):(inicio + periodos_extra)], nrow = 1)
-  mod_list <- list(B = G, U = U, Q = W, 
-                   Z = array(F[1,,(periodos_extra+1):(inicio + periodos_extra)], 
-                             c(1,m,length(F[1,1,(periodos_extra+1):(inicio + periodos_extra)]))), 
-                   A = A, R = V, x0 = mt, V0 = Ct)
-  
-  dlm <- MARSS(y_iter, model = mod_list, method = 'BFGS')
-  
-  #G_salida <-  matrix(list(0), m, m)
-  #diag(G_salida) <- dlm$par$B
-  G_salida <- matrix(c(dlm$par$B[1], 0, 0, 
-                       0, dlm$par$B[2], 0, 
-                       0,0,1), nrow=3)
-  mode(G_salida) <- 'numeric'
-  
-  # W_salida <-  matrix(list(0), m, m)
-  # diag(W_salida) <- dlm$par$Q
-  W_salida <- matrix(c(dlm$par$Q[1], 0, 0, 
-                       0, dlm$par$Q[2], 0, 
-                       0,0,0), nrow=3)
-  mode(W_salida) <- 'numeric'
-  
-  V_salida <-  coef(dlm, type = "matrix")$R
-  mode(V_salida) <- 'numeric'
-  
-  return(list('G_estimada' = G_salida, 'W_estimada' = W_salida, 'V_estimada' = V_salida))
-  
-}
-
-
-
-actualizacion_dlm_V_desc_estima_G_W_v2 <- function(y, variables_F, y_hist, variables_F_hist,
-                                                inicio, m0, C0, G_forma, W_forma, S0, n0,
-                                                lista_interv){
-  
-  error_varianza <- function(x) {
-    if(any (diag(x)<0)) stop ('Elementos del parametro de escala o varianzas deben ser positivos')
-  }
-  
-  mt_hist <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/m1_m12.rds')
-  Ct_hist <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/C1_C12.rds')
-  mt_menos_1 <- m0
-  Ct_menos_1 <- C0
-  St_menos_1 <- S0
-  nt_menos_1 <- n0
-  lista_at <- list()
-  lista_Rt <- list()
-  lista_ft <- list()
-  lista_Qt <- list()
-  lista_mt <- list()
-  lista_Ct <- list()
-  lista_CI <- list()
-  lista_CI_inf <- list()
-  lista_CI_sup <- list()
-  lista_St <- list()
-  interv <- F
-  G <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/G.rds')
-  W <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/W.rds')
-  
-  for(t in 1:length(y)){
-  
-   
-    if(t %in% lista_interv$t_int){
-      at <- lista_interv$at_int[[match(t,list_interv$t_int)]]
-      Rt <- lista_interv$Rt_int[[match(t,list_interv$t_int)]]
-      interv <- T
-    } else {
-      at <- G %*% mt_menos_1
-      Rt <- G %*% Ct_menos_1 %*% t(G) + W
-    }
-    
-    Ft <- as.numeric(variables_F[t,])
-    ft <- t(Ft) %*% at
-    Qt <- t(Ft) %*% Rt %*% Ft + St_menos_1
-    CI <- c(qst(0.025, nu = nt_menos_1, mu = ft, sigma = sqrt(Qt)),
-            qst(0.975, nu = nt_menos_1, mu = ft, sigma = sqrt(Qt)))
-    CI_inf <- CI[1]
-    CI_sup <- CI[2]
-    Yt <- y[t]
-    At <- Rt %*% Ft %*% solve(Qt)
-    et <- Yt-ft
-    mt <- at + At %*% et
-    nt <- nt_menos_1 + 1
-    St <- as.numeric(St_menos_1 + (St_menos_1/nt) * (et^2 %*% solve(Qt) - 1))
-    Ct <- (St/St_menos_1)*(Rt - At %*% Qt %*% t(At))
-    
-    error_varianza(Rt)
-    error_varianza(St)
-    error_varianza(Ct)
-    
-    lista_at[[t]] <- at
-    lista_Rt[[t]] <- Rt
-    lista_ft[[t]] <- ft
-    lista_Qt[[t]] <- Qt
-    lista_CI[[t]] <- CI
-    lista_CI_inf[[t]] <- CI_inf
-    lista_CI_sup[[t]] <- CI_sup
-    lista_mt[[t]] <- mt
-    lista_Ct[[t]] <- Ct
-    lista_St[[t]] <- St
-    mt_menos_1 <- mt
-    Ct_menos_1 <- Ct
-    nt_menos_1 <- nt
-    St_menos_1 <- St
-    
-    mt_hist[[inicio + t]] <- mt
-    Ct_hist[[inicio + t]] <- Ct
-    
-    interv <- F
-    
-    mt_inicio <- matrix(unlist(mt_hist[length(mt_hist)-inicio+1]), nrow=3)
-    Ct_inicio <- matrix(unlist(Ct_hist[length(Ct_hist)-inicio+1]), nrow=3)
-    
-    estima_MARSS <- estimacion_MARSS2(y_hist, variables_F_hist, inicio, t, G_forma,
-                                     W_forma, V = matrix(St_menos_1), mt_inicio, Ct_inicio)
-    
-    G <- estima_MARSS$G_estimada
-    W <- estima_MARSS$W_estimada
   }
   return(list("at" = lista_at,  "Rt" = lista_Rt,  "ft" = lista_ft, 
               "Qt" = lista_Qt, "CI" = lista_CI, "CI_inf" = lista_CI_inf, 
--- src/modelos/pronosticos/modelo_12_est_reordena.R
+++ src/modelos/pronosticos/modelo_12_est_reordena.R
@@ -40,7 +40,10 @@ ggplot(datos_efectivo, aes(fecha, efectivo)) +
 # Estacionalidad ------------------------------------------------------------
 
 datos_estacionalidad <- datos_efectivo %>% 
-  mutate(Q4 = ifelse(fecha - floor(fecha) == 0.75,1,0))
+  mutate(Q1 = 1) %>% 
+  mutate(Q2=0) %>% 
+  mutate(Q3=0) %>% 
+  mutate(Q4=0)
 
 
 ## number of periods of data
@@ -49,59 +52,54 @@ TT <- length(datos_efectivo$efectivo)
 
 ## get predictor variable
 pib <- matrix(datos_pib$pib, nrow=1)
+q1 <- matrix(datos_estacionalidad$Q1, nrow = 1)
+q2 <- matrix(datos_estacionalidad$Q2, nrow = 1)
+q3 <- matrix(datos_estacionalidad$Q3, nrow = 1)
 q4 <- matrix(datos_estacionalidad$Q4, nrow = 1)
 
 ## number of regr params (slope + intercept)
-m <- 3
+m <- 6
 
 ## for process eqn
 G <- matrix(list(0), m, m)
 G[1,1] <- 'G.1'
 G[2,2] <- 'G.2'
-G[3,3] <- 1
+G[3,4] <- 1
+G[4,5] <- 1
+G[5,6] <-1
+G[6,3] <- 1
 W <- matrix(list(0), m, m) ## 2x2; all 0 for now #Es Wt
-#diag(W) <- c("w.1", "w.2", 'w.3','w.4') ## 2x2; diag = (q1,q2)
 W[1,1] <- 'W.1'
 W[2,2] <- 'W.2'
-W[3,3] <- 0
 
 
 ## for observation eqn
 F <- array(NA, c(1, m, TT)) ## NxMxT; empty for now #Es Ft transpuesta
 F[1, 1, ] <- rep(1, TT) ## Nx1; 1's for intercept
 F[1, 2, ] <- pib ## Nx1; predictor variable
-F[1, 3, ] <- q4 ## Nx1; predictor variable
-
+F[1, 3, ] <- q1 ## Nx1; predictor variable
+F[1, 4, ] <- q2 ## Nx1; predictor variable
+F[1, 5, ] <- q3 ## Nx1; predictor variable
+F[1, 6, ] <- q4 ## Nx1; predictor variable
 
 y_hist <- matrix(datos_efectivo$efectivo, nrow = 1)
 
-source('src/funciones/funciones_sin_tiie.R')
+source('src/funciones/funciones_dlm_estacional_reordena.R')
 
 Vt <- matrix("v")
 
-C0 <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/C0.rds')
+C0 <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/C0.rds')
 
-m0 <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/m0.rds')
+m0 <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/m0.rds')
 
 datos_F <- datos_pib %>% 
-  filter(fecha >= 2004) %>% 
-  left_join(datos_estacionalidad %>% 
-              filter(fecha >= 2004)) %>% 
+  left_join(datos_estacionalidad) %>% 
   mutate(intercept = 1) %>% 
-  dplyr::select(5,2,4)
+  dplyr::select(8,2,4:7)
 
 datos_efectivo_y <- datos_efectivo %>% 
   filter(fecha >= 2004)
 
-list_interv <- list("t_int" = list(23), "at_int" = list(matrix(c(4.3085240,
-                                                                 0.2674386,
-                                                                 0,
-                                                                 0.1267197), nrow=4)), 
-                    "Rt_int" = list(matrix(c(9.260235e-04, -4.131049e-05, 0,    0,
-                                             -4.131049e-05,  5.294323e-06, 0,    0,
-                                             0, 0,  0,    0,
-                                             0.000000e+00,  0.000000e+00,  0.000000e+00,    0
-                    ), nrow=4)))
 
 dlm_1 <- actualizacion_dlm_estima_G_W(y = datos_efectivo_y$efectivo, variables_F = datos_F,
                                       y_hist = y_hist, variables_F_hist = F, inicio = 12,
@@ -162,14 +160,14 @@ ggplot(df_params, aes(x=fecha, y = valor)) +
 ##
 ################################################################################
 
-V <- read_rds('cache/outputs_modelos/12_periodos/sin tiie/V.rds')
+V <- read_rds('cache/outputs_modelos/12_periodos/est_reordena/V.rds')
 
 
 dlm_2 <- actualizacion_dlm_V_desc_estima_G_W(y = datos_efectivo_y$efectivo, 
                                              variables_F = datos_F,
                                              y_hist = y_hist, variables_F_hist = F, inicio = 12,
                                              m0 = m0, C0 = C0, G_forma = G, W_forma = W, 
-                                             lista_interv = list(), S0 = as.numeric(0.00006940448), n0 = 12)
+                                             lista_interv = list(), S0 = as.numeric(V), n0 = 12)
 
 
 df_graficas2 <- data.frame("fecha" = datos_efectivo_y %>% dplyr::select(fecha), 
@@ -222,35 +220,8 @@ ggplot(df_params2, aes(x=fecha, y = valor)) +
 
 
 
-################################################################################
-## Intervenciones
-##
-################################################################################
-
-
-list_interv <- list("t_int" = list(60, 66), "at_int" = list(matrix(c(3.4331459,
-                                                                 0.3888267,
-                                                                 0.09), nrow=3),
-                                                            matrix(c(3.7,
-                                                                     0.3857346,
-                                                                     0.09), nrow=3)), 
-                    "Rt_int" = list(dlm_2$Rt[[60]], dlm_2$Rt[[66]]))
-
-dlm_3 <- actualizacion_dlm_V_desc_estima_G_W(y = datos_efectivo_y$efectivo, 
-                                             variables_F = datos_F,
-                                             y_hist = y_hist, variables_F_hist = F, inicio = 12,
-                                             m0 = m0, C0 = C0, G_forma = G, W_forma = W, 
-                                             lista_interv = list_interv, S0 = as.numeric(0.00006940448), n0 = 12)
-
-
-df_graficas3 <- data.frame("fecha" = datos_efectivo_y %>% dplyr::select(fecha), 
-                           "y_real" = datos_efectivo_y$efectivo, 
-                           "y_pronostico" = dlm_3$ft %>% unlist(), "CI_inf" = dlm_3$CI_inf %>% unlist(),
-                           "CI_sup" = dlm_3$CI_sup %>% unlist()) %>% 
-  mutate(fecha = as.numeric(fecha))
-
-
-ggplot(data = df_graficas3, aes(x = fecha)) +
+ggplot(data = df_graficas2 %>% 
+         mutate(across(y_real:CI_sup, ~exp(.))), aes(x = fecha)) +
   geom_point(aes(y = y_real, shape = "Observaciones"), size = 2) +
   geom_line(aes(y = y_pronostico, color = 'Pronósticos'), size = 1) +
   geom_line(aes(y = CI_inf), color = "blue", alpha = 0.3) +
@@ -272,25 +243,82 @@ ggplot(data = df_graficas3, aes(x = fecha)) +
         axis.title = element_text(size = 22),
         legend.text = element_text(size=20)) 
 
+resids2 <- df_graficas2 %>% 
+  mutate(resids = y_real - y_pronostico) %>% 
+  dplyr::select(resids)
+
+acf(resids2)
 
 
+################################################################################
+## Intervenciones
+##
+################################################################################
 
-df_params3 <- data.frame(reduce(dlm_3$at, cbind) %>% t(), fecha = df_graficas3$fecha)  %>% 
-  rename(Intercepto = X1, PIB = X2, Q4 = X3) %>% 
-  pivot_longer(names_to = "parametro", values_to = "valor", 
-               cols = c(Intercepto, PIB, Q4))
-
-ggplot(df_params3, aes(x=fecha, y = valor)) +
-  geom_line() +
-  facet_wrap(~parametro, nrow = 4, scales = "free") +
-  theme_bw()  +
-  ylab("Valor") +
-  xlab("Fecha") +
-  theme(axis.text.x = element_text(size = 20),
-        axis.text.y = element_text(size = 20),
-        axis.title = element_text(size = 22),
-        strip.text = element_text(size=20)) 
-
+# 
+# list_interv <- list("t_int" = list(60, 66), "at_int" = list(matrix(c(3.4331459,
+#                                                                      0.3888267,
+#                                                                      0.09), nrow=3),
+#                                                             matrix(c(3.7,
+#                                                                      0.3857346,
+#                                                                      0.09), nrow=3)), 
+#                     "Rt_int" = list(dlm_2$Rt[[60]], dlm_2$Rt[[66]]))
+# 
+# dlm_3 <- actualizacion_dlm_V_desc_estima_G_W(y = datos_efectivo_y$efectivo, 
+#                                              variables_F = datos_F,
+#                                              y_hist = y_hist, variables_F_hist = F, inicio = 12,
+#                                              m0 = m0, C0 = C0, G_forma = G, W_forma = W, 
+#                                              lista_interv = list_interv, S0 = as.numeric(0.00006940448), n0 = 12)
+# 
+# 
+# df_graficas3 <- data.frame("fecha" = datos_efectivo_y %>% dplyr::select(fecha), 
+#                            "y_real" = datos_efectivo_y$efectivo, 
+#                            "y_pronostico" = dlm_3$ft %>% unlist(), "CI_inf" = dlm_3$CI_inf %>% unlist(),
+#                            "CI_sup" = dlm_3$CI_sup %>% unlist()) %>% 
+#   mutate(fecha = as.numeric(fecha))
+# 
+# 
+# ggplot(data = df_graficas3, aes(x = fecha)) +
+#   geom_point(aes(y = y_real, shape = "Observaciones"), size = 2) +
+#   geom_line(aes(y = y_pronostico, color = 'Pronósticos'), size = 1) +
+#   geom_line(aes(y = CI_inf), color = "blue", alpha = 0.3) +
+#   geom_line(aes(y = CI_sup), color = "blue", alpha = 0.3) + 
+#   geom_ribbon(aes(ymax = CI_sup, ymin = CI_inf, fill = 'Intervalo al 95%'), alpha = 0.3) +
+#   theme_bw() +
+#   scale_colour_manual(
+#     name = "", values = c("Intervalo al 95%" = "transparent",
+#                           "Pronósticos" = "black")) +
+#   scale_fill_manual(
+#     name = "",  values = c("Intervalo al 95%" = "blue",
+#                            "Pronósticos" = "transparent")) +
+#   theme(legend.position = "bottom") +
+#   labs(shape = "") +
+#   ylab('Circulación') +
+#   xlab('Fecha') +
+#   theme(axis.text.x = element_text(size = 20),
+#         axis.text.y = element_text(size = 20),
+#         axis.title = element_text(size = 22),
+#         legend.text = element_text(size=20)) 
+# 
+# 
+# 
+# 
+# df_params3 <- data.frame(reduce(dlm_3$at, cbind) %>% t(), fecha = df_graficas3$fecha)  %>% 
+#   rename(Intercepto = X1, PIB = X2, Q4 = X3) %>% 
+#   pivot_longer(names_to = "parametro", values_to = "valor", 
+#                cols = c(Intercepto, PIB, Q4))
+# 
+# ggplot(df_params3, aes(x=fecha, y = valor)) +
+#   geom_line() +
+#   facet_wrap(~parametro, nrow = 4, scales = "free") +
+#   theme_bw()  +
+#   ylab("Valor") +
+#   xlab("Fecha") +
+#   theme(axis.text.x = element_text(size = 20),
+#         axis.text.y = element_text(size = 20),
+#         axis.title = element_text(size = 22),
+#         strip.text = element_text(size=20)) 
+# 
 
 
 ggplot(data = df_graficas3 %>% 
@@ -316,7 +344,7 @@ ggplot(data = df_graficas3 %>%
         axis.title = element_text(size = 22),
         legend.text = element_text(size=20)) 
 
-resids3 <- df_graficas3 %>% 
+resids3 <- df_graficas2 %>% 
   mutate(resids = y_real - y_pronostico) %>% 
   dplyr::select(resids)
 
